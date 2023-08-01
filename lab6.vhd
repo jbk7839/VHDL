d4367298303c07db5da860d993d850312b56a145
jbk7839 <55522378+jbk7839@users.noreply.github.com>
2023-08-01 21:35:12 +0900

    Lectuer.2 MUX
    Design 4-to1 MUX in two ways using VHDL

--- lab6.vhd
+++ lab6.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity lab6 is
-	port(		x,y,z : 		 in std_logic;
-				f1,f2,f3,f4: out std_logic );
+	port(		x,y,z :  in std_logic;
+		  f1,f2,f3,f4 : out std_logic );
 end lab6;
 
 architecture my_lab6 of lab6 is
@@ -14,7 +14,7 @@ begin
 with sel select
 	f1 <= '1' when "000" | "001" | "110" | "111"
 	f2 <= '1' when "001" | "010" | "011" | "110" | "111"
-	f3	<= '1' when "011" | "101" | "110" | "111" 
+	f3 <= '1' when "011" | "101" | "110" | "111" 
 	f4 <= '1' when "000" | "010" | "011" | "100" | "110"
-			'0' when others;
+	      '0' when others;
 end architecture;
